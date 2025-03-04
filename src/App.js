a53aafb8821f87da30d22b47c65ced3b73a65eb6
Zaheer <32474533+zaheersani@users.noreply.github.com>
2025-03-04 14:31:31 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-// function App() {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -25,4 +25,4 @@ import './App.css';
   );
 }
 
-// export default App;
+export default App;
